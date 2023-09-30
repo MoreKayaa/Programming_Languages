1af6075b0f3d9500b852d095cf7ee6d95c032255
Favian Mokaya <125963580+MoreKayaa@users.noreply.github.com>
2023-09-30 12:16:30 +0300

    Add files via upload

---
+++ Sum Calculator.py
@@ -0,0 +1,8 @@
+#Favian Mokaya Imbera SCT211-0022/2021
+
+#Enter your name and two numbers
+name = str(input())
+num1 = int(input())
+num2 = int(input())
+sum = num1 + num2
+print ("Hello, "+name+". The sum of "+str(num1)+" and "+str(num2)+" is "+str(sum))
